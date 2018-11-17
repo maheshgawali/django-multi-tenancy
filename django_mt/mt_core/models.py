4ccbfc84b77d0cead356712f87c61fb502d10628
mahesh gawali <maheshgawali@gmail.com>
2018-11-17 14:54:59 +0530

    added user and db detail models

--- django_mt/mt_core/models.py
+++ django_mt/mt_core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
+import uuid
 
-# Create your models here.
+
+class BaseDateTimeModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseDateTimeUUIDModel(BaseDateTimeModel):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class DbDetails(BaseDateTimeUUIDModel):
+    engine = models.CharField(max_length=191)
+    name = models.CharField(max_length=191)
+    user = models.CharField(max_length=191)
+    host = models.CharField(max_length=191)
+    port = models.CharField(max_length=191)
+    password = models.CharField(max_length=191)
+    options = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return 'DbDetails:{}-{}'.format(self.uuid, self.name)
+
+
+class UserDb(BaseDateTimeUUIDModel):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    db = models.ForeignKey(DbDetails, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('user', 'db'),)
+
+    def __str__(self):
+        return 'UserDb:{}-user({})-db({})'.format(self.uuid, self.user, self.db)
