2c8737f29d99e59e3f0a59853bc2fee0f2536e4a
mahesh gawali <maheshgawali@gmail.com>
2018-11-17 15:12:55 +0530

    updated dbdetails to use port no as int and added test_data for mt_core

--- django_mt/mt_core/models.py
+++ django_mt/mt_core/models.py
@@ -24,7 +24,7 @@ class DbDetails(BaseDateTimeUUIDModel):
     name = models.CharField(max_length=191)
     user = models.CharField(max_length=191)
     host = models.CharField(max_length=191)
-    port = models.CharField(max_length=191)
+    port = models.IntegerField()
     password = models.CharField(max_length=191)
     options = models.TextField(null=True, blank=True)
 
@@ -33,10 +33,6 @@ class DbDetails(BaseDateTimeUUIDModel):
 
 
 class UserDb(BaseDateTimeUUIDModel):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     db = models.ForeignKey(DbDetails, on_delete=models.CASCADE)
 
